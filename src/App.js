e0f6e2c6f642c6aac017e29d4db8b504936f6053
RodrigoZambrana <rodrigo@lacnic.net>
2022-02-07 17:52:25 -0300

    ontener productos desde API

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import React from "react";
 import Header from "./components/page/Header";
 import Footer from "./components/page/Footer";
 import Body from "./components/page/Body";
+import useFetch from "./hooks/useFetch";
+import { url_json_costos } from "./utils/variables";
 
 function App() {
+  const getCostosProductos = useFetch(url_json_costos);
+  console.log(getCostosProductos);
+
   return (
     <div className="App">
       <Header />
---
+++ src/utils/variables.js
@@ -0,0 +1,1 @@
+export const url_json_costos = "http://myjson.dit.upm.es/api/bins/etd5";
