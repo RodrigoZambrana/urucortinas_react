5d546dd4a8706924f6281487910fbf6625ad14cb
RodrigoZambrana <rodrigo@lacnic.net>
2022-05-28 14:15:35 -0300

    funciona obtener productos por id

--- src/components/page/Constantes.js
+++ src/components/page/Constantes.js
@@ -1,13 +1,13 @@
-export const RUTA_API = "http://localhost:8888/bd/";
-export const urlApiProducts = "https://api.myjson.com/bins/12nb8n"; // http://myjson.com/12nb8n
-export const BASE_PATH = "http://localhost:3000";
-export const STORAGE_PRODUCTS_CART = "productsCart";
+export const RUTA_API = 'http://localhost:8888/bd'
+export const urlApiProducts = 'https://api.myjson.com/bins/12nb8n' // http://myjson.com/12nb8n
+export const BASE_PATH = 'http://localhost:3000'
+export const STORAGE_PRODUCTS_CART = 'productsCart'
 
 export const opcionesSelectPresupuesto = [
-  { id: 0, text: "Seleccione una opción" },
-  { id: 1, text: "Roller" },
-  { id: 2, text: "Bandas Verticales" },
-  { id: 3, text: "Veneciana" },
-  { id: 4, text: "Catalana PVC" },
-  { id: 5, text: "Catalana Aluminio" },
-];
+  { id: 0, text: 'Seleccione una opción' },
+  { id: 1, text: 'Roller' },
+  { id: 2, text: 'Bandas Verticales' },
+  { id: 3, text: 'Veneciana' },
+  { id: 4, text: 'Catalana PVC' },
+  { id: 5, text: 'Catalana Aluminio' },
+]
